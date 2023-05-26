ba8ef7d41127f781c43ddd9163eb93c45e1922b5
ankur <frost.ankur019@gmail.com>
2023-05-26 13:50:02 +0530

    done

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const path = require("path");
 
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: "https://apnii-dukaan.netlify.app",
     credentials: true,
     methods: ["POST", "GET", "DELETE", "PUT"],
   })
